beff29298ff689666b788bd8cb713a73888cf344
Anas Ibn Belal <yorder07@gmail.com>
2024-08-16 16:28:24 +0600

    chore: Update CORS configuration to allow specified origins and simplify vercel.json

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://filepanel.vercel.app",
+  "https://filepanel.vercel.app"
 ];
 
 const corsOptions = {
-  origin: "*",
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
