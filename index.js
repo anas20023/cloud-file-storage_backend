3fbab2f4b74014e0c11dc31d81c67672fe2ad0df
Anas Ibn Belal <yorder07@gmail.com>
2024-08-16 16:16:16 +0600

    chore: Update CORS configuration to allow any origin and simplify vercel.json

--- index.js
+++ index.js
@@ -13,15 +13,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
